0c4ed50ba2ddf74fe42046302642653dbe0d8917
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-10-29 11:06:52 +0800

    Release 0.1.7

--- bash.sh
+++ bash.sh
@@ -12,10 +12,27 @@ else
     exit 1
 fi
 echo 'Updating version number'
+if [ -z $py ]; then
+    echo 'Error: Please input python command: python or python3'
+    echo 'exit...'
+    exit 1
+fi
+if [ -z $old_version ]; then
+    echo 'Error: Please input old version'
+    echo 'exit...'
+    exit 1
+fi
+if [ -z $new_version ]; then
+    echo 'Error: Please input new version'
+    echo 'exit...'
+    exit 1
+fi
 echo old version: $old_version
 echo new version: $new_version
 sed -i '' -e "s/$old_version/$new_version/" src/loon/__init__.py
 echo 'Creating new version'
+git add .
+git commit -m "Release $new_version"
 git tag $new_version
 echo 'Creating some distributions'
 $py setup.py sdist bdist_wheel
--- src/loon/__init__.py
+++ src/loon/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-__version__ = "0.1.5"
+__version__ = "0.1.7"
 __author__ = "ShixiangWang"
 __copyright__ = "ShixiangWang"
 __license__ = "mit"
